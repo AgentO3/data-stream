631bfc1517c8162e3cc3e9ca861003ae439aae05
Owen VC_LAST_NAME <owen@vividcortex.com>
2015-05-16 18:44:03 -0400

    Append to dom by element

--- app/js/main.js
+++ app/js/main.js
@@ -57,7 +57,7 @@ function init() {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-    document.body.appendChild( renderer.domElement );
+    document.getElementById("data-data-mountains").appendChild( renderer.domElement );
 
 }
 
@@ -90,9 +90,6 @@ function animate() {
     }
 
     mesh.geometry.verticesNeedUpdate = true;
-
-    console.log(terrain);
-
 
 }
 
